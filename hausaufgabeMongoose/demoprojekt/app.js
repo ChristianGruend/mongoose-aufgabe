fdb0a24fb739c85c377a6ab252bca53603673f68
Gruender Christian <122011772+ChristianGruend@users.noreply.github.com>
2023-06-19 16:37:29 +0200

    teil1_fertig

--- hausaufgabeMongoose/demoprojekt/app.js
+++ hausaufgabeMongoose/demoprojekt/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 //Importiere das Schrauben Model
+const Schraube = require('./models/Schraube');
+
 
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('//MONGODB ATLAS STRING', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://christiangruender:8fBQzZdDlLX1kBE4@cluster0.xhnylat.mongodb.net/schrauben24?retryWrites=true&w=majority",
+ { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
   console.log('MongoDB connected...');
 })
@@ -16,9 +20,25 @@ app.get('/', (req, res) => {
 });
 
 // Route eine Schraube
-  
-// Route alle Schrauben
+app.get('/sales/:id', async (req, res) => {
+  try {
+    const sales = await Schraube.find({ produkt_id: req.params.id });
+    res.json(sales);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
+
+// Route alle Schrauben
+app.get('/sales', async (req, res) => {
+  try {
+    const sales = await Schraube.find();
+    res.json(sales);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
